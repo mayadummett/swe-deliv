e7401ff90bfda1c056b557522bad2827e3f75187
Maya Dummett <67915049+mayadummett@users.noreply.github.com>
2023-02-05 18:51:45 -0500

    fixing non running code

--- src/utils/mutations.js
+++ src/utils/mutations.js
@@ -11,7 +11,6 @@ export const emptyEntry = {
    description: "",
    user: "",
    category: 0,
-   need: 0,
 }
 
 export async function addEntry(entry) {
@@ -21,7 +20,6 @@ export async function addEntry(entry) {
       description: entry.description,
       user: entry.user,
       category: entry.category,
-      need: entry.need,
       // The ID of the current user is logged with the new entry for database user-access functionality.
       // You should not remove this userid property, otherwise your logged entries will not display.
       userid: entry.userid,
@@ -36,7 +34,6 @@ export async function updateEntry(entry) {
       link: entry.link,
       description: entry.description,
       category: entry.category,
-      need: entry.need,
    });
 }
 
